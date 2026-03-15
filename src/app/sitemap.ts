2ab53327ff0bdfe0d3ca4d64f661d6497d9ec073
Gautam Kumar Chaudhary <gautamkr.chaudhary11@gmail.com>
2026-03-16 03:51:51 +0530

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,5 +4,13 @@ export default function sitemap() {
       url: "https://your-project.vercel.app",
       lastModified: new Date(),
     },
+    {
+      url: "https://your-project.vercel.app/create",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://your-project.vercel.app/receive",
+      lastModified: new Date(),
+    },
   ];
 }
